388210cdc52b846c39a797bb26537b5108414e4c
satyaPanigrahy9639 <aws.satya03@gmail.com>
2026-04-04 16:52:02 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+       HashSet<String> hs = new HashSet<>();
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j< board[0].length; j++){
+                if(board[i][j] == '.')
+                    continue;
+                if(!hs.add(board[i][j]+" in row "+i))
+                    return false;
+                if(!hs.add(board[i][j]+" in column "+j))
+                    return false;
+                if(!hs.add(board[i][j]+" in sub "+i/3+" and "+j/3))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
